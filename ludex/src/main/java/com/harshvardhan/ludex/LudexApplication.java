2c32344bea2e6d6d80aad9b70350042c57e32858
Harshvardhan <harshvardhanti12@gmail.com>
2026-09-13 20:49:58 +0530

    initial commit

---
+++ ludex/src/main/java/com/harshvardhan/ludex/LudexApplication.java
@@ -0,0 +1,13 @@
+package com.harshvardhan.ludex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudexApplication.class, args);
+	}
+
+}
---
+++ ludex/src/test/java/com/harshvardhan/ludex/LudexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshvardhan.ludex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
