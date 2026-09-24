46a73c794a98ce51e76ea0f2c9ecd01bebfefc3e
Harshvardhan <harshvardhanti12@gmail.com>
2026-09-24 22:38:06 +0530

    Remove setters from ErrorResponse, keep getters only

--- ludex/src/main/java/com/harshvardhan/ludex/exception/GlobalExceptionHandler.java
+++ ludex/src/main/java/com/harshvardhan/ludex/exception/GlobalExceptionHandler.java
@@ -3,18 +3,39 @@ package com.harshvardhan.ludex.exception;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GameNotFoundException.class)
-    public ResponseEntity<String> handleGameNotFound(GameNotFoundException e) {
+    public ResponseEntity<ErrorResponse> handleGameNotFound(GameNotFoundException e) {
+
+        ErrorResponse error = new ErrorResponse(404,
+                e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
+                .body(error);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(
+            MethodArgumentNotValidException e) {
+
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        ErrorResponse error = new ErrorResponse(
+                400,
+                message);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(error);
+    }
 }
